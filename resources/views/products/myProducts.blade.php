e27462549f924a403dc8fc8a0ce47b88ccd12e0b
Jesse Gordijn <129741847+TheCodeMaestro@users.noreply.github.com>
2025-02-04 17:02:49 +0100

    Product styling gemaakt

--- resources/views/products/myProducts.blade.php
+++ resources/views/products/myProducts.blade.php
@@ -1,53 +1,54 @@
 <x-app-layout>
-    <section class="product-container-tijdelijk">
-        <section class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+    <!-- <section class="product-container-tijdelijk">
+        <section class="mt-6 bg-white shadow-sm rounded-lg divide-y"> -->
             @foreach ($products as $product)
                 @if($product->owner->is(Auth::user()))
-                    <section class="p-6 flex space-x-2">
-                        <section class="flex-1">
-                            <section class="flex justify-between items-center">
-                                <section>
-                                    <h2>{{ $product->name }}</h2>
-                                    <p>Category: {{ $product->category }}</p>
-                                    <p>Description: {{ $product->description }}</p>
-                                    <p>Status: {{ $product->status }}</p>
-                                    <p>Owner: {{ $product->user->name }}</p>
-                                    @if($product->image_path)
-                                        <img src="{{ asset('storage/' . $product->image_path) }}" alt="Product Image">
-                                    @endif
-                                    <small class="text-sm text-gray-600">Posted on: {{ $product->created_at->format('j M Y, g:i a') }}</small>
-                                    <!-- <form method="POST" action="{{ route('products.destroy', $product->id) }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <x-primary-button class="mt-4">Delete product</x-primary-button>
-                                    </form> -->
-
-                                    <x-dropdown>
-                                        <x-slot name="trigger">
-                                            <button>
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                </svg>
-                                            </button>
-                                        </x-slot>
-                                        <x-slot name="content">
-                                            <form method="POST" action="{{ route('products.destroy', $product->id) }}">
-                                                @csrf
-                                                @method('delete')
-                                                <x-dropdown-link :href="route('products.destroy', $product)" onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Delete') }}</x-dropdown-link>
-                                            </form>
-                                        </x-slot>
-                                    </x-dropdown>
-
-                                </section>
+                    <section class="product-box">
+                        <section class="product-header1">
+                            <h2>{{ $product->name }}</h2>
+                            <p>Category: {{ $product->category }}</p>
+                        </section>
+                        <section class="product-header2">
+                            <p>Owner: {{ $product->user->name }}<br>
+                            Posted on: {{ $product->created_at->format('j M Y, g:i a') }}<br>
+                            Status: {{ $product->status }}</p>
+                            <section>
+                                <x-dropdown align='top'>
+                                    <x-slot name="trigger">
+                                        <button>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="options-icon" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        <form method="POST" action="{{ route('products.destroy', $product->id) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-dropdown-link :href="route('products.destroy', $product)" onclick="event.preventDefault(); 
+                                            this.closest('form').submit();">{{ __('Delete') }}</x-dropdown-link>
+                                        </form>
+                                    </x-slot>
+                                </x-dropdown>
                             </section>
+                        </section>
+                        <section class="product-description">
+                            <p>{{ $product->description }}</p>
+                        </section>
+                        <section class="product-image">
+                            @if($product->image_path)
+                                <img src="{{ asset('storage/' . $product->image_path) }}" alt="Product Image">
+                            @else
+                                <h2 style="text-align: center">No image available</h2>
+                            @endif
                         </section>
                     </section>
                 @endif    
             @endforeach
-    </section>
-    </section>
-    <section class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <!-- </section>
+    </section> -->
+
+    <section class="product-form-tijdelijk">
         <form method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
             @csrf
             <section>
